86b04e21b4fbb4bf0a4bab9898ce30df9a05dc83
Dufrane Alain <alduf@live.be>
2021-12-16 10:20:22 +0100

    recettes omelettes

--- FridgeProject/listeFrigo.py
+++ FridgeProject/listeFrigo.py
@@ -9,7 +9,6 @@ paprika = Aliments.objects.get(name="Paprika")
 basilic = Aliments.objects.get(name="Basilic")
 curry = Aliments.objects.get(name="Curry")
 laitConcentre =Aliments.objects.get(name="Lait concentré")
-huileOlives  = Aliments.objects.get(name="Huile d'Olives")
 cubeBouillon = Aliments.objects.get(name="Cube de bouillon")
 persil = Aliments.objects.get(name='Persil')
 cassonade = Aliments.objects.get(name="Cassonade")
@@ -32,9 +31,7 @@ filetDePoulet = Aliments.objects.get(name="Filets de poulet")
 herbesDeProvence = Aliments.objects.get(name="Herbes de Provence") 
 cremeFraiche = Aliments.objects.get(name="Crème fraiche")
 reblechon = Aliments.objects.get(name="Reblechon")
-beurre = Aliments.objects.get(name="Beurre")
 lait = Aliments.objects.get(name="Lait")
-gruyereRape = Aliments.objects.get(name="Gruyère râpé")
 parmesanrapa = Aliments.objects.get(name="Parmesan rapé")
 jambon = Aliments.objects.get(name="Jambon")
 pates = Aliments.objects.get(name="Pâtes")
@@ -51,15 +48,12 @@ choufleur= Aliments.objects.get(name="Choux fleur")
 chouDeBruxelles= Aliments.objects.get(name="Choux de Bruxelles")
 endive = Aliments.objects.get(name="Endive")
 haricot = Aliments.objects.get(name="Haricot")
-ail= Aliments.objects.get(name="Ail")
 oignon = Aliments.objects.get(name="Oignons")
 echalote = Aliments.objects.get(name="Echalote")
 orange = Aliments.objects.get(name="Orange")
 vermouth=Aliments.objects.get(name="Verliytg
 tomate = Aliments.objects.ger(name="Tomate"
-sel= Aliments.objects.get(name="Sel")
-poivre=Aliments.objects.get(name="Poivre")
-lardons=Aliments.objects.get(name="Lardons")
+
 
 
 
@@ -79,3 +73,56 @@ gratinDeChouFleur.aliments.add(farine,cubeBouillon,sel,poivre,muscade,chouxFleur
 gratinDeChouFleur.save()
 
 
+
+ciboulette = Aliments(name="Ciboulette")
+ciboulette.save()
+ciboulette = Aliments.objects.get(name="Ciboulette")
+huileOlives  = Aliments.objects.get(name="Huile d'Olives")
+gruyereRape = Aliments.objects.get(name="Gruyère râpé")
+courgette = Aliments.objects.get(name="Courgettes")
+sel= Aliments.objects.get(name="Sel")
+poivre=Aliments.objects.get(name="Poivre")
+lardons=Aliments.objects.get(name="Lardons")
+beurre = Aliments.objects.get(name="Beurre")
+ail= Aliments.objects.get(name="Ail")
+oeufs = Aliments.objects.get(name="Oeufs")
+pommeDeTerre = Aliments.objects.get(name="Pomme de Terre")
+
+
+
+omeletteNature=Recettes(name="Omelette nature")
+omeletteNature.save()
+omeletteNature=Recettes.objects.get(name="Omelette nature")
+omeletteNature.picture="https://assets.afcdn.com/recipe/20100120/22445_w768h583c1cx256cy192.webp"
+omeletteNature.howToCook="https://www.marmiton.org/recettes/recette_omelette-nature_21255.aspx"
+omeletteNature.save()
+omeletteNature.aliments.add(sel,poivre,oeuf,beurre)
+omeletteNature.save()
+
+
+omelettePdtLardons=Recettes(name="Omelette aux pommes de terre et lardons")
+omelettePdtLardons.save()
+omelettePdtLardons=Recettes.objects.get(name="Omelette aux pommes de terre et lardons")
+omelettePdtLardons.picture="https://assets.afcdn.com/recipe/20200220/107931_w1200h800c1cx2880cy1920cxb5760cyb3840.webp"
+omelettePdtLardons.howToCook="https://www.marmiton.org/recettes/recette_omelette-aux-pommes-de-terre-et-lardons_19387.aspx"
+omelettePdtLardons.save()
+omelettePdtLardons.aliments.add(sel,poivre,oeufs,pommeDeTerre,lardons,huileOlive)
+omelettePdtLardons.save()
+
+omeletteCourgette=Recettes(name="Omelette aux courgettes râpées")
+omeletteCourgette.save()
+omeletteCourgette=Recettes.objects.get(name="Omelette aux courgettes râpées")
+omeletteCourgette.picture="https://assets.afcdn.com/recipe/20190410/90647_w768h583c1cx3963cy2690cxb6000cyb4000.webp"
+omeletteCourgette.howToCook="https://www.marmiton.org/recettes/recette_omelette-aux-courgettes-rapees_60317.aspx"
+omeletteCourgette.save()
+omeletteCourgette.aliments.add(gruyereRape,sel,oeufs,huileOlive,courgette,ciboulette,ail)
+omeletteCourgette.save()
+
+=Aliments(name="")
+.save()
+=Aliments.objects.get()
+.picture=''
+.howToCook=""
+.save()
+.aliments.add()
+.save()
